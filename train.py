62dabc7af9932765f76782bcd45a839c8d4a5c13
Muhammad Sahal Mulki <56467460+sahal-mulki@users.noreply.github.com>
2023-01-13 15:28:24 +0400

    Update train.py

--- train.py
+++ train.py
@@ -179,7 +179,7 @@ model.compile(loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=T
 
 #And finally, train the model for 4 epochs.
 
-history = model.fit(train_data, epochs=args.epochs[0], validation_data = val_data, callbacks=[tensorboard_callback])
+history = model.fit(train_data, epochs=args.epochs[0], validation_data = val_data)
 
 print("")
 print("Training done! Saving model with name SpottingDiffusion.....")
